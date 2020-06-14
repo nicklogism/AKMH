1ae97922fa842fc959b0d5d2c77b03be94022d60
Neon Cube <57285367+nicklogism@users.noreply.github.com>
2020-06-14 22:03:58 +0300

    Update SQLQuery1.sql

--- B6/SQL/11.06.2020/SQLQuery1.sql
+++ B6/SQL/11.06.2020/SQLQuery1.sql
@@ -1,9 +1,9 @@
 /* Πως ορίζουμε μεταβλητή και αναθέτουμε τιμή */
 
-DECLARE @myvar INT /* Ορίζουμε τη μεταβλητή ως integer */
+DECLARE @myvar INT; /* Ορίζουμε τη μεταβλητή ως integer */
 
-SET @myvar = 20
+SET @myvar = 20;
 
-SELECT @myvar /* Για εμφάνιση στα αποτελέσματα */
+SELECT @myvar; /* Για εμφάνιση στα αποτελέσματα */
 
-PRINT @myvar /*για να το εμφανίσω στα messages */  
+PRINT @myvar; /*για να το εμφανίσω στα messages */  
