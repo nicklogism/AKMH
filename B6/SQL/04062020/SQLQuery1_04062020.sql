1a4bfc99b28557fb14ba0a5ba3b6be129de70a1f
Neon Cube <57285367+nicklogism@users.noreply.github.com>
2020-06-14 22:01:10 +0300

    Update SQLQuery1_04062020.sql

--- B6/SQL/04062020/SQLQuery1_04062020.sql
+++ B6/SQL/04062020/SQLQuery1_04062020.sql
@@ -1,3 +1,3 @@
 SELECT FirstName, MiddleName, LastName /*Επιλογή μόνο των FirstName, MiddleName, LastName*/
 FROM person.person /*από πίνακα person.person */
-WHERE LastName like 'sy%'; /*Που είναι(Φέρε) όλα τα αποτελέσματα με επώνυμο που αρχίζει από sy*/
+WHERE LastName like 'sy%';  /*Που είναι(Φέρε) όλα τα αποτελέσματα με επώνυμο που αρχίζει από sy*/
