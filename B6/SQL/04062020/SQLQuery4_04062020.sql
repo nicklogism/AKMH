c631a2710e9e55d7e5f643cc2ff117d4a919f3f1
Neon Cube <57285367+nicklogism@users.noreply.github.com>
2020-06-14 22:00:29 +0300

    Update SQLQuery4_04062020.sql

--- B6/SQL/04062020/SQLQuery4_04062020.sql
+++ B6/SQL/04062020/SQLQuery4_04062020.sql
@@ -1,4 +1,5 @@
 SELECT salesorderid,SUM(linetotal) /*Επιλογή κλειδιών (salesorderid) , άθροισμα της linetotal */
 FROM sales.salesorderdetail
-GROUP BY salesorderid /* Επιλογή από τον πίνακα sales.SalesOrderDetail*/
+GROUP BY salesorderid; 
+/* Επιλογή από τον πίνακα sales.SalesOrderDetail*/
 /*¨Οποτε έχω ένα aggregate function ΟΛΑ τα πεδία που ΔΕΝ είναι aggregated πρέπει να μπουν σε Group.*/
