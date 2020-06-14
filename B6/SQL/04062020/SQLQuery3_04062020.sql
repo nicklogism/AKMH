a6c3f21c3c5107cf0000ba57c626544b8855a044
Neon Cube <57285367+nicklogism@users.noreply.github.com>
2020-06-14 22:00:47 +0300

    Update SQLQuery3_04062020.sql

--- B6/SQL/04062020/SQLQuery3_04062020.sql
+++ B6/SQL/04062020/SQLQuery3_04062020.sql
@@ -1,3 +1,3 @@
 SELECT FirstName, MiddleName, LastName /*Επιλογή μόνο των FirstName, MiddleName, LastName*/
 FROM person.person /*από πίνακα person.person */
-WHERE LastName LIKE '%s'  /* Φέρνει όσα τελειώνουν σε s . Όσα ξεκινάνε από s θα τα ζητάγαμε με s% */
+WHERE LastName LIKE '%s';  /* Φέρνει όσα τελειώνουν σε s . Όσα ξεκινάνε από s θα τα ζητάγαμε με s% */
