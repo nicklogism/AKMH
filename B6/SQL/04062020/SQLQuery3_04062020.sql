f9ce41aeb7ca567bba56b0e1a93985354656774f
Neon Cube <57285367+nicklogism@users.noreply.github.com>
2020-06-14 21:57:56 +0300

    Update and Rename

---
+++ B6/SQL/04062020/SQLQuery3_04062020.sql
@@ -0,0 +1,3 @@
+SELECT FirstName, MiddleName, LastName /*Επιλογή μόνο των FirstName, MiddleName, LastName*/
+FROM person.person /*από πίνακα person.person */
+WHERE LastName LIKE '%s'  /* Φέρνει όσα τελειώνουν σε s . Όσα ξεκινάνε από s θα τα ζητάγαμε με s% */
