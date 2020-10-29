80716e33e231b6f057dc7833357ec9ea356416e4
Neon Cube <57285367+nicklogism@users.noreply.github.com>
2020-10-29 19:03:53 +0200

    Create php-variables.php

---
+++ C6/PHP-HTML/29.10.2020/php-variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP Variables</title>
+	<style>
+		h1{
+			text-align:center;
+		}
+		
+		div{
+			border:1px solid gray;
+			padding:10px;
+			margin-bottom:10px;
+		}
+	</style>
+</head>
+
+<body>
+
+<h1> PHP Variables </h1>
+
+
+</body>
+</html>
