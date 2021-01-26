68177528d91973542c73693dcb8f7c24c1806025
Nick Logism <nicklogism@gmail.com>
2021-01-26 21:32:12 +0200

    UpdateLesson9Poll
    New table with percentage and graph,  new SQL query included.

--- C6/PHP/Lesson_9_Poll/index.php
+++ C6/PHP/Lesson_9_Poll/index.php
@@ -32,6 +32,11 @@ $message = '';
             text-align: center;
         }
 
+        #poll {
+            margin-left: auto;
+            margin-right: auto;
+        }
+
         .even {
             background: whitesmoke;
         }
@@ -41,7 +46,7 @@ $message = '';
         }
 
         #submit {
-            display: inline-block;
+            display: block;
             padding: 10px 20px;
             font-size: 24px;
             cursor: pointer;
@@ -56,7 +61,7 @@ $message = '';
         }
 
         .centered {
-            position: fixed;
+            position: absolute;
             top: 57%;
             left: 47%;
         }
@@ -65,6 +70,11 @@ $message = '';
             text-align: center;
             padding-top: 10%;
             font-weight: bold;
+        }
+
+        .bara {
+            background-image: linear-gradient(to right, yellow, orange, red);
+            height: 16px;
         }
     </style>
 </head>
@@ -75,12 +85,11 @@ $message = '';
     <h3> Ψηφίστε το πιο Cool Game Ever</h3>
 
     <form action="<?php print  $_SERVER['PHP_SELF'] ?>" method="post">
-        <table id="poll" cellpadding="10" align="center" width="60%">
+        <table id="poll" cellpadding="10" width="60%">
 
             <thead>
                 <tr>
                     <th>Game</th>
-                    <th id="votes">Votes</th>
                 </tr>
             </thead>
 
@@ -115,9 +124,6 @@ $message = '';
                         <input name="id" type="radio" value="<?= $id ?>">
                         <?= $name ?>
                     </td>
-                    <td id="votes" class='<?= $bg ?>'>
-                        <?php echo $votes ?>
-                    </td>
                 </tr>
             <?php
             } // end while
@@ -132,6 +138,60 @@ $message = '';
     <div class="message">
         <?= $message ?>
     </div>
+
+    <table id="poll" cellpadding="10" width="60%">
+
+<thead>
+    <tr>
+        <td> <b>GAME</b> </td>
+        <td>  </td>
+        <td> </td>
+        
+    </tr>
+</thead>
+
+<?php
+
+if ($_POST) {
+    $id = $_REQUEST['id'];
+
+    $query = "UPDATE poll SET votes = votes+1 WHERE id = $id";
+    //echo $query;  
+    mysqli_query($conn, $query) or die($query . "  " . mysqli_error($conn));
+    $message = "VOTE SUBMITED SUCCESFULLY";
+}
+
+$bg = '';
+$votes_sum_query = "SELECT sum(votes) as sum_votes FROM poll ";
+$res = mysqli_query($conn, $votes_sum_query) or die(mysqli_error($conn));
+$row = mysqli_fetch_assoc($res);
+$totalVotes = $row['sum_votes'];
+$query = "SELECT * FROM poll";
+$res = mysqli_query($conn, $query) or die(mysqli_error($conn));
+while ($row = mysqli_fetch_assoc($res)) {
+    $name       = $row['name'];
+    $votes      = $row['votes'];
+    $percentage = number_format((($votes/$totalVotes) * 100), 2);
+    $graphWidth = round($percentage).'px'; 
+
+    if ($bg == 'even') {
+        $bg = 'odd';
+    } else {
+        $bg = 'even';
+    }
+?>
+
+    <tr>
+        <td class='<?= $bg ?>'>
+            <?= $name ?>
+        </td>
+        <td> <div class="bara" style="width:<?= $graphWidth ?>" >  </div> </td>
+        <td> <?= $percentage ?>% </td>
+    </tr>
+<?php
+} // end while
+?>
+</table>
 </body>
 
 </html>
